f38cf5c4d19076567bfeb46d632d600ec78d02f0
jpVinnie <janpaul.ramos@protonmail.com>
2023-08-31 21:59:37 -0400

    finish first part

---
+++ L2/birthday.ts
@@ -0,0 +1,15 @@
+function main(n: number) {
+  var c: number = probability(n);
+  console.log(23);
+}
+
+function probability(n: number): number {
+  var prob: number = 1;
+
+    for (let i = 1; i < n; i = i+1) {
+        var log: number = 365 - i
+        var logUpdated: number = log/365
+        prob = prob * logUpdated
+    }
+    return 1 - prob * 100 / 100;
+  }
