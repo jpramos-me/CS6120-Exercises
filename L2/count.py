5059f73756993d792201e3d1ac1fda49937c1d5c
jpVinnie <janpaul.ramos@protonmail.com>
2023-08-31 22:35:07 -0400

    argument counter

---
+++ L2/count.py
@@ -0,0 +1,18 @@
+import json
+import sys
+from collections import Counter
+
+def argAmount(file):
+
+	for f in file['functions']:
+		for i in f['instrs']:
+			if "args" in i:
+				if (len(i['args']) == 0):
+				  print("No arguments given")
+				else:
+					for a in i['args']:
+						print(a)
+
+if __name__ == '__main__':
+	file = open(sys.argv[1])
+	argAmount(json.load(file))
