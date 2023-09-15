b31c5fe9c3dd8fa71182898075ef465d1a0cce78
jpVinnie <janpaul.ramos@protonmail.com>
2023-09-14 23:54:39 -0400

    remove ocaml code

--- L3/util.ml
+++
@@ -1,9 +0,0 @@
-open Core
-open Yojson
-
-(** split_blocks json takes a Bril generated JSON file
-    and gets the set of instructions and outputs the list
-    of blocks. *)
-let split_blocks json = 
-  let string list = [] in 
-  
